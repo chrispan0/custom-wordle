3d000a4184ef24011bb54b3f15d5748f2774324c
Phineas Ziegler <phinziegler@gmail.com>
2022-02-22 08:10:31 -0500

    idk, just needed the updated files on my laptop so im commiting

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 //  WORDLE
-
-let state = [
-    ["_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_"],
-    ["_", "_", "_", "_", "_"],
-];
 const gridSpace = 2.5; // 5px border between tiles
+
+function generateEmptyState(tries, wordlength) {
+    let output = [];
+    for(let r = 0; r < tries; r++) {
+        let row = [];
+        for(let c = 0; c < wordlength; c++) {
+            row.push(" ");
+        }
+        output.push(row);
+    }
+    return output;
+}
 
 function generateBoard(state) {    // default is 6x5
     let board = document.getElementById("board");
@@ -16,40 +19,57 @@ function generateBoard(state) {    // default is 6x5
         let rowDiv = document.createElement("div");
         rowDiv.classList.add("row");
         for (let col = 0; col < state[row].length; col++) {
-            let div = createDiv(row, col);
+            let div = createDiv(row, col, calculateWidth(state));
             rowDiv.appendChild(div);
         }
         board.appendChild(rowDiv);
     }
 }
 
-function createDiv(row, col) {
+function createDiv(row, col, width) {
     let div = document.createElement("div");
     div.id = tileID(row, col);
     div.classList.add("tile");
-    let width = calculateWidth(state);
     div.style.width = width + "px";
     div.style.height = width + "px";
     div.style.margin = gridSpace + "px";
+    // div.style.fontSize = width / 2 + "px";
+
+    let p = createTextElem(row, col, width)
+    div.appendChild(p);
     return div;
+}
+
+function createTextElem(row, col, width) {
+    let p = document.createElement("p");
+    p.id = textID(row, col);
+    p.classList.add("tileText");
+    p.innerText = "I";
+    p.style.fontSize = width / 1.5 + "px";
+    return p;
 }
 
 function tileID(row, col) {
     return "tile[" + row + ", " + col + "]";
 }
 
-function getDiv(row, col) {
-    return getElementById(tileID(row, col));
+function textID(row, col) {
+    return "text[" + row + ", " + col + "]";
+}
+
+function getTile(row, col) {
+    return document.getElementById(tileID(row, col));
+}
+
+function getTileText(row, col) {
+    return document.getElementById(textID(row, col));
 }
 
 function calculateWidth(state) {
     let margin = gridSpace;
-    console.log(margin);
-    // let rows = state.length;
     let cols = state[0].length;
     let bWidth = boardWidth();
     let width = (bWidth / cols) - (2 * margin);
-    // console.log(width);
     return width;
 }
 
@@ -57,5 +77,14 @@ function boardWidth() {
     return board.clientWidth;
 }
 
+function editText(row, col, text) {
+    getTileText(row, col).innerText = text;
+}
+
+// RUNTIME
+
+let state = generateEmptyState(6, 5);
 generateBoard(state);
+
+// editText(2, 4, "0");
 
